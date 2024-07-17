1de680d4bd5b084c9920a95063c731802fe0a31a
Xuan Phao <nguyenthanhhoa075@gmail.com>
2024-07-17 17:34:56 +0700

    lang="en"

--- bai_tap_HTML_DOM/main.js
+++ bai_tap_HTML_DOM/main.js
@@ -24,41 +24,70 @@ Cho trước file HTML, các bạn hãy sử dụng các phương thức truy v�
 // const productsListElement = document.getElementsByClassName("products-list");
 // console.log(productsListElement[0]);
 
-// const firstProductElement = document.querySelector(".products-list "); ⛔???
-// console.log(firstProductElement("div")); 😡
+// const firstProductElement = document.querySelector(".products-list .product:first-child");
+// console.log(firstProductElement);
 
-// const buttonElements = document.querySelectorAll(".cta-btn");
+// const buttonElements = document.getElementsByClassName("cta-btn");
 // console.log(buttonElements);
 
 // ⭐ Bài 139. Thực hành với DOM attribute:
 /* Cho sẵn file HTML, các bạn hãy thực hiện các yêu cầu sau:
-Thêm thuộc tính title có giá trị "F8 - Học lập trình để đi làm" cho thẻ h1.
-Thêm thuộc tính data-title có giá trị "F8 - Học lập trình để đi làm" cho thẻ h1.
+Thêm thuộc tính title có giá trị "F8 - Học lập trình để đi làm" cho thẻ h3.
+Thêm thuộc tính data-title có giá trị "F8 - Học lập trình để đi làm" cho thẻ h3.
 Thêm thuộc tính href có giá trị "https://fullstack.edu.vn/" cho thẻ a.
 Thêm thuộc tính target có giá trị "_blank" cho thẻ a. */
 
-// const titleEl = document.querySelector("a");
+// const titleEl = document.querySelector("h3");
+// console.log(titleEl);
 // titleEl.title = "F8 - Học lập trình để đi làm";
 
-// titleEl.data-title="F8 - Học lập trình để đi làm"; 😡
+// titleEl.setAttribute("data-title", "F8 - Học lập trình để đi làm");
 
-// titleEl.href = "https://fullstack.edu.vn/";
-// titleEl.target = "_blank";
-// console.log(titleEl);
+// const linkEl = document.querySelector("a");
+// linkEl.href = "https://fullstack.edu.vn/"; // Cách 1
+// linkEl.setAttribute("target", "_blank"); // Cách 2
+// console.log(linkEl);
 
 // ⭐ Bài 141. Thực hành sử dụng InnerText:
-// Các bạn hãy thay đổi nội dung của thẻ h1 thành F8 - Học lập trình để đi làm.
+// Các bạn hãy thay đổi nội dung của thẻ h2 thành F8 - Học lập trình để đi làm.
 
 // const headingElement1 = document.querySelector("h2");
-// headingElement1.innerText = "F8 - Học lập trình để đi làm"; // ⛔???
+// headingElement1.innerText = "F8 - Học lập trình để đi làm";
 
 // ⭐ Bài 143. Thực hành sử dụng InnerHTML #1:
 // Các bạn hãy viết hàm render nhận vào 1 tham số là html, hàm render sẽ có nhiệm vụ chèn giá trị của html vào trong thẻ ul đã cho trước.
 
-// const boxEl = document.querySelector("ul");
-// console.log((boxEl.innerHTML = "<li>🌺☘💕🌻🌼</li>"));
+// const item = "<li>🌺☘💕🌻🌼</li>";
+
+// const render = (html) => {
+//   const boxEl = document.querySelector("ul");
+//   boxEl.innerHTML = html;
+// };
+
+// render(item);
 
 // ⭐ Bài 144. Thực hành sử dụng InnerHTML #2:
+/*
+Các bạn hãy viết hàm render có 1 tham số courses, hàm render sẽ thêm các item của mảng courses để tạo thành 1 danh sách các khóa học trên giao diện.
+Ví dụ: Với mảng var courses = ['ReactJS', 'AngularJS', 'VueJS'] sẽ thu được kết quả:
+  ReactJS
+  AngularJS
+  VueJS
+Gợi ý:
+Sử dụng phương thức map kết hợp với join để tạo chuỗi HTML từ mảng courses.
+Gán chuỗi HTML vừa tạo vào thuộc innerHTML của thẻ ul giống bài trước nhé.
+*/
+
+// const courses = ["HTML & CSS", "Javascript", "PHP", "Java"];
+/* const renderCourses = (courses) => {
+  const menu = document.querySelector(".courses-list");
+  const elements = courses.map((course) => {
+    const newItem = `<li>${course}</li>`;
+    return newItem;
+  });
+  menu.innerHTML = elements.join("");
+};
+renderCourses(courses); */
 
 // ⭐ Bài 139. Thực hành với DOM attribute:
 // ⭐ Bài 139. Thực hành với DOM attribute:
